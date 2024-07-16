988b7584aea2233eb4b5910e7bdf06bc435a7743
admin@86maid.com <admin@86maid.com>
2024-07-17 01:35:19 +0800

    dll

--- src/lib.rs
+++ src/lib.rs
@@ -1627,18 +1627,20 @@ mod tests {
         let mut ddddocr = ddddocr_classification_cuda(114514).unwrap();
     }
 
-    // #[test]
-    // fn diy_model() {
-    //     let mut ddddocr = Ddddocr::with_model_charset(
-    //         r#"C:\Users\XChuang233\Downloads\ddddocr_models-main\cpdaily\model\cpdaily.onnx"#,
-    //         r#"C:\Users\XChuang233\Downloads\ddddocr_models-main\cpdaily\model\charsets.json"#,
-    //     )
-    //     .unwrap();
+    #[test]
+    fn diy_model() {
+        let mut ddddocr = Ddddocr::with_model_charset(
+            r#"C:\Users\XChuang233\Downloads\ddddocr_models-main\cpdaily\model\cpdaily.onnx"#,
+            r#"C:\Users\XChuang233\Downloads\ddddocr_models-main\cpdaily\model\charsets.json"#,
+        )
+        .unwrap();
 
-    //     let result = ddddocr.classification(
-    //         include_bytes!(r#"C:\Users\XChuang233\Downloads\ddddocr_models-main\cpdaily\images\飞机_8cb1df1a25a84a8ba9f767d6875f414f.jpg.jpg"#),
-    //         false);
+        let result = ddddocr.classification(
+            include_bytes!(r#"
+            "#),
+            false,
+        );
 
-    //     println!("{:?}", result);
-    // }
+        println!("{:?}", result);
+    }
 }
